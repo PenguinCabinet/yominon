8a4759a6fe3e8075f07c940c5f3158613b684787
pengincoalition <kage2.kage22@gmail.com>
2021-02-26 03:33:14 +0000

    open jtalk use stdin

--- Dockerfile
+++ Dockerfile
@@ -41,6 +41,10 @@ RUN ls
 RUN npm i co
 
 ADD ./src/ /src
+RUN echo "こんにちは" | open_jtalk \
+         -x /var/lib/mecab/dic/open-jtalk/naist-jdic/ \
+         -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
+         -ow sample.wav
 
 CMD ["node","src/main.js"]
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs=require("fs");
 const execSync= require('child_process').execSync;
+const exec=require("child_process").exec;
+const spawn=require("child_process").spawn;
 
 var make_voice_task_id=0;
 var make_voice_task_id_max=128;
@@ -33,9 +35,48 @@ async function make_voice_tasks_run(){
 }
 
 
-function make_voice(text,id){
-    fs.writeFileSync(`temp${id}.txt`, text);
-    execSync(`open_jtalk temp${id}.txt -m /MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice -x /var/lib/mecab/dic/open-jtalk/naist-jdic -ow temp${id}.mp3`, (err, stdout, stderr) => {})
+async function make_voice(text,id){
+    //fs.writeFileSync(`temp${id}.txt`, text);
+    //execSync(`open_jtalk temp${id}.txt -m /MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice -x /var/lib/mecab/dic/open-jtalk/naist-jdic -ow temp${id}.mp3`, (err, stdout, stderr) => {})
+    /*execSync(
+	    `open_jtalk -m /MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice -x /var/lib/mecab/dic/open-jtalk/naist-jdic -ow temp${id}.mp3`,
+	    {stdio:text}
+    );*/
+	
+	let run_f=async function(){
+		return new Promise((resolve)=>{
+    			let open_jtalk=exec(
+		    		`open_jtalk -m /MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice -x /var/lib/mecab/dic/open-jtalk/naist-jdic -ow temp${id}.mp3`,
+				{env:process.env}
+	    		);
+
+			open_jtalk.stdout.pipe(process.stdout);
+
+			open_jtalk.stdin.setEncoding('utf-8');
+			open_jtalk.stdin.write(text);
+			open_jtalk.stdin.end();
+	
+			let is_command_end=false;
+
+			open_jtalk.on("close",(code)=>{
+				console.log("close");
+				is_command_end=true;
+				resolve();
+			});
+		});
+	};
+
+	await run_f();
+
+	/*
+	while(!is_command_end){
+		//console.log(is_command_end);
+	}*/
+
+
+		//open_jtalk.stdin.write(text);
+	//open_jtalk.stdin.end();
+	
 }
 
 client.once('ready', () => {
@@ -75,15 +116,13 @@ client.on('message', message =>
 		//console.log("TRUE4");
 		let make_voice_task_id_now=make_voice_task_id;
 		
-		make_voice_tasks.push(function(){
-			return new Promise(resolve => {
-        			make_voice(message.content,make_voice_task_id_now);
-        			const dispatcher = connection.play(`temp${make_voice_task_id_now}.mp3`);
-				resolve(message.content.length);
-			});
+		make_voice_tasks.push(async function(){
+        		await make_voice(message.content,make_voice_task_id_now)
+				
+       			const dispatcher = connection.play(`temp${make_voice_task_id_now}.mp3`);
+			return message.content.length
 		});
 		
-		//worker.postMessage({"text":,message.content,"id":make_voice_task_id_now,"BOT_KEY":process.env.BOT_KEY,"connection":connection});
 
 		make_voice_task_id++;
 		make_voice_task_id%=make_voice_task_id_max;
